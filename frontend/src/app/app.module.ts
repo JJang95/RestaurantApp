14d54f465a91ff0f77694f33cf2ac2c208c3dac6
Alex <alex404an@gmail.com>
2023-06-23 17:23:05 -0400

    Update app.module.ts
    Added the admin component to the file

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,12 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AdminComponent } from './components/admin/admin.component';
 import {AdminLoginComponent} from './components/admin-login/admin-login.component';
 import {RestaurantLoginComponent} from './components/restaurant-login/restaurant-login.component';
 import { RestaurantComponent} from './components/restaurant/restaurant.component';
 import { WelcomeComponent} from './components/welcome/welcome.component';
 import { LoginComponent} from './components/login/login.component';
-
 import { RestaurantService } from './restaurant.service';
 
 import { HttpClientModule } from '@angular/common/http';
@@ -18,6 +18,7 @@ import { AppRoutingModule } from './app-routing.module';
   declarations: [
     AppComponent,
     AdminLoginComponent,
+    AdminComponent,
     RestaurantLoginComponent,
     RestaurantComponent,
     WelcomeComponent,
