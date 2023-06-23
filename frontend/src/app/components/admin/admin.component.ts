d398fea66da0c7fedee84955420ac39078bc5fa6
Alex <alex404an@gmail.com>
2023-06-23 17:22:46 -0400

    Update admin page

--- frontend/src/app/components/admin/admin.component.ts
+++ frontend/src/app/components/admin/admin.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { Restaurant } from '../../restaurant';
+import { RestaurantUser } from '../../restaurantUser';
 import { RestaurantService } from '../../restaurant.service';
 import { HttpErrorResponse } from '@angular/common/http';
-import { NgForm} from '@angular/forms';
+import { NgForm, FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { Router } from '@angular/router';
 
 @Component({
@@ -10,103 +10,83 @@ import { Router } from '@angular/router';
   templateUrl: './admin.component.html',
   styleUrls: ['./admin.component.css']
 })
+
+
+
 export class AdminComponent implements OnInit {
-  public restaurants: Restaurant[] = [];
-  public editRestaurant: Restaurant | undefined;
-  public deleteRestaurant: Restaurant | undefined;
+  public restaurantUsers: RestaurantUser[] = [];
+  public editRestaurantUser: RestaurantUser | undefined;
+  public deleteRestaurantUser: RestaurantUser | undefined;
 
   failed: boolean = false;
   constructor (private router: Router, private restaurantService: RestaurantService) {}
 
   ngOnInit() {
-    this.getRestaurants();
+    // This inital process so that restaurants are loaded onto the page
+    this.getRestaurantUser();
   }
 
-  public getRestaurants(): void {
-    this.restaurantService.getRestaurants().subscribe(
-      (response: Restaurant[]) => {
-        this.restaurants = response;
+//Call this method will reload the page with an update of the restaurant list
+  public getRestaurantUser(): void{
+    this.restaurantService.getRestaurantUsers().subscribe(
+      (response: RestaurantUser[]) => {
+        this.restaurantUsers = response;
       },
-      (error: HttpErrorResponse) => {
+      (error: HttpErrorResponse) =>{
         alert(error.message);
       }
-    );
+    )
   }
-
-  public onAddRestaurant(addForm: NgForm): void {
+  public onAddRestaurantUser(addForm: NgForm): void{
+    console.log("HELLO ADD USER");
     document.getElementById('add-restaurant-form')?.click();
-    this.restaurantService.addRestaurant(addForm.value).subscribe(
-      (response: Restaurant) => {
+    this.restaurantService.addRestaurantUser(addForm.value).subscribe(
+      (response: RestaurantUser) =>{
         console.log(response);
-        this.getRestaurants();
+        this.getRestaurantUser();
         addForm.reset();
+
       },
-      (error: HttpErrorResponse) => {
+      (error: HttpErrorResponse) =>{
         alert(error.message);
         addForm.reset();
       }
-    );
+    )
   }
-
-  public onUpdateRestaurant(restaurant: Restaurant): void {
-    this.restaurantService.updateRestaurant(restaurant).subscribe(
-      (response: Restaurant) => {
-        console.log(response);
-        this.getRestaurants();
-      },
-      (error: HttpErrorResponse) => {
-        alert(error.message);
-      }
-    );
-  }
-
-  public onDeleteRestaurant(restaurantId: number): void {
-    this.restaurantService.deleteRestaurant(restaurantId).subscribe(
+  public onDeleteRestaurantUser(r_UserId: number): void{
+    this.restaurantService.deleteRestaurantUser(r_UserId).subscribe(
       (response: void) => {
         console.log(response);
-        this.getRestaurants();
+        this.getRestaurantUser();
       },
-      (error: HttpErrorResponse) => {
+      (error: HttpErrorResponse) =>{
         alert(error.message);
       }
-    );
+    )
   }
+public onOpenModal(restaurantUser: RestaurantUser|null, mode:string): void{
+  const container = document.getElementById('main-container');
+  const button = document.createElement('button');
+  button.type = 'button';
+  button.style.display = 'none';
+  button.setAttribute('data-toggle','modal');
+  if(mode==='add'){
+    console.log("ADD");
+    button.setAttribute('data-target', '#addRestaurantUserModal');
+  }
+  if(mode === 'edit'){
+    this.editRestaurantUser = restaurantUser || undefined;
+  }
+  if(mode === 'delete'){
+    this.deleteRestaurantUser = restaurantUser || undefined;
+    console.log("DELETE");
+    button.setAttribute('data-target', '#deleteRestaurantUserModal');
+  }
+  container?.appendChild(button);
+  button.click();
+}
 
-  public searchRestaurants(key: string): void {
-    const results: Restaurant[] = [];
-    for(const restaurant of this.restaurants) {
-      if (restaurant.name.toLowerCase().indexOf(key.toLowerCase()) !== -1
-      || restaurant.type.toLowerCase().indexOf(key.toLowerCase()) !== -1
-      || restaurant.price.toString().indexOf(key.toLowerCase()) !== -1) {
-        results.push(restaurant)
-      }
-    }
-    this.restaurants = results;
-    if (results.length === 0 || !key) {
-      this.getRestaurants();
-    }
-  }
 
-  public onOpenModal(restaurant: Restaurant|null, mode: string): void {
-    const container = document.getElementById('main-container');
-    const button = document.createElement('button');
-    button.type = 'button';
-    button.style.display = 'none';
-    button.setAttribute('data-toggle', 'modal');
-    if (mode === 'add') {
-      button.setAttribute('data-target', '#addRestaurantModal');
-    }
-    if (mode === 'edit') {
-      this.editRestaurant = restaurant || undefined;
-      button.setAttribute('data-target', '#updateRestaurantModal');
-    }
-    if (mode === 'delete') {
-      this.deleteRestaurant = restaurant || undefined;
-      button.setAttribute('data-target', '#deleteRestaurantModal');
-    }
-    container?.appendChild(button);
-    button.click();
-  }
 
   async logout(){
     let resp = this.restaurantService.logout();
@@ -122,13 +102,5 @@ export class AdminComponent implements OnInit {
       alert(error.message);
     }
   );
-
-  //
-  // (response: this.restaurantService.logout()) => {
-  //   this.restaurants = response;
-  // },
-  // (error: HttpErrorResponse) => {
-  //   alert(error.message);
-  // }
   }
 }
